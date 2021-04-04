6509fcaf72ff742830203afd652f1f891d11e3fc
claudiospinelli93 <claudiomanoel93@gmail.com>
2021-04-03 23:31:53 -0300

    ✅ Add unit test.

---
+++ test/Service.Test/CalculatorTest.cs
@@ -0,0 +1,35 @@
+using System;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Service.Test
+{
+    public class CalculatorTest
+    {
+
+        private readonly AutoMocker _mocker;
+        private readonly Calculator _calculator;
+        public CalculatorTest()
+        {
+            _mocker = new AutoMocker();
+
+            _calculator = _mocker.CreateInstance<Calculator>();
+        }
+        
+        [Fact]
+        public void Sum_CorrectValue_ReturnSum()
+        {
+            // arrange
+            int value1 = 2;
+            int value2 = 3;
+
+            // act
+            var result = _calculator.Sum(value1, value2);
+
+            // assert
+            Assert.Equal(5, result);
+        }
+
+        
+    }
+}
