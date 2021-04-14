ae4af99cdc925cb539b64bcaf8654ef938c557ed
claudiospinelli93 <claudiomanoel93@gmail.com>
2021-04-14 16:56:19 -0300

    ❌ remove test.

--- test/Service.Test/CalculatorTest.cs
+++ test/Service.Test/CalculatorTest.cs
@@ -15,16 +15,6 @@ namespace Service.Test
 
             _calculator = _mocker.CreateInstance<Calculator>();
         }
-                [Fact]
-        public void Sum_FailValue_Exception()
-        {
-            // arrange
-            int value1 = -2;
-            int value2 = 3;
-
-            // act/assert
-            var exception = Assert.Throws<Exception>(() => _calculator.Sum(value1, value2));
-            Assert.Equal("Field value1 or value2 less than 0.", exception.Message);
-        }      
+     
     }
 }
