8cbcfc35318c7f3a32aea448bbdfef6e5e4ff0aa
claudiospinelli93 <claudiomanoel93@gmail.com>
2021-04-14 15:55:43 -0300

    ❌ remove unit test.

--- test/Service.Test/CalculatorTest.cs
+++ test/Service.Test/CalculatorTest.cs
@@ -15,19 +15,16 @@ namespace Service.Test
 
             _calculator = _mocker.CreateInstance<Calculator>();
         }
-        
-        [Fact]
-        public void Sum_CorrectValue_ReturnSum()
+                [Fact]
+        public void Sum_FailValue_Exception()
         {
             // arrange
-            int value1 = 2;
+            int value1 = -2;
             int value2 = 3;
 
-            // act
-            var result = _calculator.Sum(value1, value2);
-
-            // assert
-            Assert.Equal(5, result);
-        }        
+            // act/assert
+            var exception = Assert.Throws<Exception>(() => _calculator.Sum(value1, value2));
+            Assert.Equal("Field value1 or value2 less than 0.", exception.Message);
+        }      
     }
 }
